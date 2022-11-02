e169a4ffb5992d4ff621e1f935c81054043bffa0
VitaliyLenin <vitaliylenin77@gmail.com>
2022-11-03 00:40:32 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,25 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+const createLi = (element) => {
+  const ingredientItem = document.createElement("li");
+  ingredientItem.textContent = element;
+  ingredientItem.classList.add("item");
+
+  // console.log(ingredientItem);
+  return ingredientItem;
+};
+
+const list = ingredients.map((ingredient) => createLi(ingredient));
+
+ingredientsList.append(...list);
+
+// console.log(list);
