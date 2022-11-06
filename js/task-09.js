e19756cd0a06a108d93cfa3126a33585032f9490
VitaliyLenin <vitaliylenin77@gmail.com>
2022-11-07 01:06:26 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  changeColorBtn: document.querySelector(".change-color"),
+  backColorName: document.querySelector(".color"),
+  body: document.querySelector("body"),
+};
+
+refs.changeColorBtn.addEventListener("click", onChangeColorBack);
+
+function onChangeColorBack(event) {
+  const backgroundColor = getRandomHexColor();
+  refs.body.style.backgroundColor = backgroundColor;
+  refs.backColorName.textContent = backgroundColor;
+}
