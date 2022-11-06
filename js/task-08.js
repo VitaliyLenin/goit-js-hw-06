4d1591ce111edd58891ad624095d4ea7c53b626f
VitaliyLenin <vitaliylenin77@gmail.com>
2022-11-07 00:44:05 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const refs = {
+  form: document.querySelector(".login-form"),
+};
 
+refs.form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Всі поля мають бути заповнені");
+  } else {
+    event.currentTarget.reset();
+  }
+}
